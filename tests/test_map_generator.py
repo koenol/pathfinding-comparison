a54584a7ec2046738044c596718e71eeab3b86b6
koenol <olli.s.koskinen@helsinki.fi>
2026-03-18 05:48:49 +0200

    added pytest-dev to poetry

--- tests/test_map_generator.py
+++ tests/test_map_generator.py
@@ -1,9 +1,14 @@
 """Tests for map generation"""
 
 import unittest
+from src.map_generator import MapGenerator
 
 class TestMapGenerator(unittest.TestCase):
-	pass
+	def setUp(self):
+		self.map_generator = MapGenerator()
+
+	def test_cell_size(self):
+		self.assertEqual(self.map_generator.cell_size, 1)
 
 if __name__ == "__main__":
 	unittest.main()
