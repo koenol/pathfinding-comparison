25e9b5fe6df12575644ddd70feeeb0375f8dfe01
koenol <olli.s.koskinen@helsinki.fi>
2026-03-20 07:26:41 +0200

    added astar class, pathfinder is not fully functional yet

---
+++ src/astar.py
@@ -0,0 +1,67 @@
+"""A* Algorithm:
+f(n) = g(n) + h(n)
+g(n) = cost of path from the start node to n
+h(n) = heuristic calc from n to goal
+"""
+import math
+from queue import PriorityQueue
+
+class Astar:
+	"""A* pathfinder"""
+	BLOCKED_TILES = {"@", "O", "T"}
+	def __init__(self):
+		"""Init. A*"""
+		self.running = False
+
+	def start_pathfinder(self):
+		"""Set pathfinder status to True"""
+		self.running = True
+
+	def stop_pathfinder(self):
+		"""Set pathfinder status to False"""
+		self.running = False
+
+	def h(self, point1, point2):
+		"""Calculate Manhattan distance"""
+		x1, y1 = point1
+		x2, y2 = point2
+		return abs(x1 - x2) + abs(y1 - y2)
+
+	def is_in_bounds(self, grid, point):
+		"""Check bounds"""
+		x, y = point
+		rows = len(grid)
+		cols = len(grid[0])
+		return 0 <= x < cols and 0 <= y < rows
+
+	def is_passable(self, grid, point):
+		"""Check whether tile is passable"""
+		x, y = point
+		return grid[y][x] not in self.BLOCKED_TILES
+
+	def get_neighbors(self, grid, point):
+		"""Return valid neighbor tiles"""
+		x, y = point
+		neighbors = []
+		directions = [
+			(1, 0),
+			(-1, 0),
+			(0, 1),
+			(0, -1),
+		]
+
+		for dx, dy in directions:
+			next_point = (x + dx, y + dy)
+			if self.is_in_bounds(grid, next_point) and self.is_passable(grid, next_point):
+				neighbors.append(next_point)
+		return neighbors
+
+	def move_cost(self, point1, point2):
+		"""Return movement movement cost"""
+		dx = abs(point1[0] - point2[0])
+		dy = abs(point1[1] - point2[1])
+		return math.sqrt(2) if dx == 1 and dy == 1 else 1
+
+	def find_path(self, grid, start, goal):
+		"""Start A* pathfinder"""
+		pass
