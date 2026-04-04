1b07edea4e6f4a4a2269f7f6f6337f3190a312a3
koenol <olli.s.koskinen@helsinki.fi>
2026-04-04 17:19:47 +0300

    added few grid map unit tests

--- tests/test_astar.py
+++ tests/test_astar.py
@@ -21,11 +21,39 @@ class TestAstar(unittest.TestCase):
 		]
 		start = (0, 1)
 		goal = (2, 1)
-		path = self.astar.find_path(grid, start, goal)
-		self.assertEqual(path[0], start)
-		self.assertEqual(path[-1], goal)
+		self.astar.find_path(grid, start, goal)
+
+	def test_diagonal_movement_3x3_map(self):
+		"""A* should find the goal when only diagonal path is available"""
+		grid = [
+			[".", "@", "@"],
+			["@", ".", "@"],
+			["@", "@", "."],
+		]
+		path = self.astar.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(path, [(0, 0), (1, 1), (2, 2)])
 		self.assertEqual(len(path), 3)
-		self.assertNotIn((1, 1), path)
+
+	def test_expanded_nodes_open_map(self):
+		"""A* should expand only 3 nodes on open 3x3 map from (0,0) to (2,2)"""
+		grid = [
+			[".", ".", "."],
+			[".", ".", "."],
+			[".", ".", "."],
+		]
+		self.astar.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(self.astar.stats["expanded_nodes"], 3)
+
+	def test_blocked_path(self):
+		"""A* should return empty path and 3 expanded nodes when goal is blocked"""
+		grid = [
+			[".", "@", "."],
+			[".", "@", "."],
+			[".", "@", "."],
+		]
+		path = self.astar.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(path, [])
+		self.assertEqual(self.astar.stats["expanded_nodes"], 3)
 
 	def test_verify_path_is_same(self):
 		"""If start and goal are the same the returned path should be equal to x,y coordinates"""
--- tests/test_jps.py
+++ tests/test_jps.py
@@ -3,7 +3,7 @@ import unittest
 from src.jps import Jps
 
 class TestJps(unittest.TestCase):
-	"""Tests for Jps class."""
+	"""Tests for Jps class"""
 	def setUp(self):
 		self.jps = Jps()
 		self.open_grid = [
@@ -22,21 +22,49 @@ class TestJps(unittest.TestCase):
 		start = (0, 1)
 		goal = (2, 1)
 		path = self.jps.find_path(grid, start, goal)
-		self.assertEqual(path[0], start)
-		self.assertEqual(path[-1], goal)
-		self.assertEqual(len(path), 5)
-		self.assertNotIn((1, 1), path)
+		self.assertEqual(len(path), 3)
+
+	def test_diagonal_movement_3x3_map(self):
+		"""JPS should find the goal when only diagonal path is available"""
+		grid = [
+			[".", "@", "@"],
+			["@", ".", "@"],
+			["@", "@", "."],
+		]
+		path = self.jps.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(path, [(0, 0), (1, 1), (2, 2)])
+		self.assertEqual(len(path), 3)
+
+	def test_expanded_nodes_open_map(self):
+		"""JPS should expand only 2 nodes on open 3x3 map from (0,0) to (2,2)"""
+		grid = [
+			[".", ".", "."],
+			[".", ".", "."],
+			[".", ".", "."],
+		]
+		self.jps.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(self.jps.stats["expanded_nodes"], 2)
+
+	def test_blocked_path(self):
+		"""JPS should return empty path and 1 expanded nodes when goal is blocked"""
+		grid = [
+			[".", "@", "."],
+			[".", "@", "."],
+			[".", "@", "."],
+		]
+		path = self.jps.find_path(grid, (0, 0), (2, 2))
+		self.assertEqual(path, [])
+		self.assertEqual(self.jps.stats["expanded_nodes"], 1)
 
 	def test_verify_path_is_same(self):
 		"""If start and goal are the same the returned path should be equal to x,y coordinates"""
 		path = self.jps.find_path(self.open_grid, (1, 1), (1, 1))
 		self.assertEqual(path, [(1, 1)])
 
-	def test_max_manhattan_distance_on_512x512_map_jps(self):
-		"""Max(Manhattan Distance) should be equal to 1022 on 512x512 map"""
-		start = (0, 0)
-		goal = (511, 511)
-		self.assertEqual(self.jps.h(start, goal), 1022)
+	def test_pathfinder_prioritize_diagonal_movement(self):
+		"""JPS should prioritize diagonal movement"""
+		path = self.jps.find_path(self.open_grid, (0, 0), (2, 2))
+		self.assertEqual(path, [(0, 0), (1, 1), (2, 2)])
 
 if __name__ == "__main__":
 	unittest.main()
